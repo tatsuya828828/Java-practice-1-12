2bec33641f98ee080a9d972a1b9a985c7d460867
tatsuya828828 <kkjy011894@gmail.com>
2020-06-25 22:57:52 +0900

    [finish]12-4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
 	public static void main(String[] args) {
+		Character c = new Wizard();
+		if (c instanceof SuperHero) {
+			SuperHero h  = (SuperHero)c;
+			h.fly();
+		}
 		Wizard w = new Wizard();
 		Matango m = new Matango();
 		w.name = "アサカ";
